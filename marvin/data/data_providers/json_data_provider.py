f5ea08a5f7e7ddf41011cc33531e4ae628e79c5c
Esteban Mendoza <estemendoza@gmail.com>
2017-02-19 20:24:55 +0100

    Fixed JSON Data Provider
    - Fixed PEP 8 issues
    - Fixed name of the class when calling super

--- marvin/data/data_providers/json_data_provider.py
+++ marvin/data/data_providers/json_data_provider.py
@@ -1,13 +1,16 @@
+"""JSON Data Provider"""
+
 import json
 
 from marvin.data.data_provider import DataProvider
+
 
 class JSONDataProvider(DataProvider):
     """
     DataDriven data provider for JSON sources.
     The data_source_id of this provider is the path to a JSON file with the following structure:
 
-        { 
+        {
           "setup": *** JSON object representing data for the 'setup' block ***,
           "iterations": [
             *** JSON object representing data for iteration 1 ('run' block) ***,
@@ -16,14 +19,14 @@ class JSONDataProvider(DataProvider):
             *** JSON object representing data for iteration N ('run' block) ***,
           ],
 
-          "tear_down": *** JSON object representing data for the 'tear_down' block *** 
+          "tear_down": *** JSON object representing data for the 'tear_down' block ***
         }
     """
-    
+
     def __init__(self, *args, **kargs):
-        super(JSONData, self).__init__(*args, **kargs)
-        with open(self._source_id, "r") as fd:
-            self.json = json.load(fd)
+        super(JSONDataProvider, self).__init__(*args, **kargs)
+        with open(self._source_id, "r") as file_descriptor:
+            self.json = json.load(file_descriptor)
 
     def setup_data(self):
         return self.json.get('setup', {})
