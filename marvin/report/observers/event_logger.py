d4d478b26e9bb8022680065bf625d72270f3cc62
Esteban Mendoza <estemendoza@gmail.com>
2017-05-22 17:29:17 -0300

    Report enhancements (#7)
    * Adding report summary, now shows a list of test cases executed, it's status and the status of the iterations executed
    
    * Fixing dependency name
    
    * Fix pep8 issue
    
    * Changed implementation of the internal status of the tests for the report summary. Also, you can change the test title on the report from the setup data
    
    * Adding changes suggested by @argos83

--- marvin/report/observers/event_logger.py
+++ marvin/report/observers/event_logger.py
@@ -1,4 +1,5 @@
 import sys
+import traceback
 
 from colorama import Fore, Back
 
@@ -11,8 +12,11 @@ COLORS = {
     Status.PASS: Fore.GREEN + '%s' + Fore.RESET,
     Status.FAIL: Fore.RED + '%s' + Fore.RESET,
     Status.SKIP: Fore.BLUE + Back.WHITE + '%s' + Back.RESET + Fore.RESET,
-    'HEADER': Fore.CYAN + '%s' + Fore.RESET
+    'HEADER': Fore.CYAN + '%s' + Fore.RESET,
+    'BLOCK': Fore.MAGENTA + '%s' + Fore.RESET
 }
+
+INDENT_CHAR = " "
 
 
 class EventLogger(object):
@@ -30,15 +34,56 @@ class EventLogger(object):
         publisher.subscribe(self.on_step_ended, EventType.STEP_ENDED)
         publisher.subscribe(self.on_test_started, EventType.TEST_STARTED)
         publisher.subscribe(self.on_test_ended, EventType.TEST_ENDED)
-        self._indent = "  "
+        publisher.subscribe(self.on_setup_started, EventType.TEST_SETUP_STARTED)
+        publisher.subscribe(self.on_setup_ended, EventType.TEST_SETUP_ENDED)
+        publisher.subscribe(self.on_iteration_started, EventType.TEST_ITERATION_STARTED)
+        publisher.subscribe(self.on_iteration_ended, EventType.TEST_ITERATION_ENDED)
+        publisher.subscribe(self.on_teardown_started, EventType.TEST_TEARDOWN_STARTED)
+        publisher.subscribe(self.on_teardown_ended, EventType.TEST_TEARDOWN_ENDED)
+        publisher.subscribe(self.on_suite_ended, EventType.SUITE_ENDED)
+
+        self._indent = INDENT_CHAR * 2
         self._o = dest
         self._color = marvin.util.files.supports_color(self._o)
+        self._suite_status = []
+        self._current_status = {}
+
+    def on_setup_started(self, event):
+        self._on_block_started('SETUP STARTED')
+
+    def on_setup_ended(self, event):
+        self._on_block_ended(event, 'SETUP FINISHED')
+
+    def on_iteration_started(self, event):
+        self._on_block_started('ITERATION STARTED')
+
+    def on_iteration_ended(self, event):
+        self._current_status["iterations"][event.status] += 1
+        self._on_block_ended(event, 'ITERATION FINISHED')
+
+    def on_teardown_started(self, event):
+        self._on_block_started('TEARDOWN STARTED')
+
+    def on_teardown_ended(self, event):
+        self._on_block_ended(event, 'TEARDOWN FINISHED')
+
+    def _on_block_started(self, title):
+        block_header = self._in_color('BLOCK', title)
+        self._indent = INDENT_CHAR * 4
+        self._p("%s[%s]", self._indent, block_header)
+
+    def _on_block_ended(self, event, title):
+        block_header = self._in_color('BLOCK', title)
+        status = self._colored_status(event.status)
+        self._p("%s[%s - %s]", self._indent, block_header, status)
+        self._indent = INDENT_CHAR * 2
 
     def on_step_started(self, event):
         step = event.step
+        tags = "(" + ", ".join(step.tags) + (")") if len(step.tags) > 0 else ""
 
         self._p("-" * 64)
-        self._p("%s%s: %s (%s)", self._indent * step.level, step.name, step.description, ", ".join(step.tags))
+        self._p("%s%s: %s %s", self._indent * step.level, step.name, step.description, tags)
 
     def on_step_ended(self, event):
         step = event.step
@@ -47,6 +92,22 @@ class EventLogger(object):
         self._p("%s[%s] %s (%d ms)", self._indent * step.level, status, step.name, event.duration)
 
     def on_test_started(self, event):
+        # Check if the test's title is defined on the setup for the summary
+        # report. If not, we use the name of test script
+        test_name = event.test_script.name
+        setup_data = event.data_provider.setup_data()
+        if setup_data.get("test_report_title"):
+            test_name = setup_data["test_report_title"]
+
+        self._current_status = {
+            "test_name": test_name,
+            "test_status": None,
+            "iterations": {
+                Status.PASS: 0,
+                Status.FAIL: 0,
+                Status.SKIP: 0
+            }
+        }
         test_script = event.test_script
         test_header = self._in_color('HEADER', 'TEST')
 
@@ -56,17 +117,35 @@ class EventLogger(object):
         test_script = event.test_script
         test_header = self._in_color('HEADER', 'TEST')
         status = self._colored_status(event.status)
+        self._current_status["test_status"] = event.status
+
+        self._suite_status.append(self._current_status)
+
+        if event.status != Status.PASS and any(event.exceptions):
+            exception = event.exceptions[0]
+            self._p(repr(exception[1]))
+            self._p(''.join(traceback.format_tb(exception[2])))
 
         self._p("-" * 64)
         self._p("[%s] %s - %s", test_header, test_script.name, status)
 
+    def on_suite_ended(self, event):
+        self._p("\n" + "-" * 64)
+        self._p("Execution Summary")
+        self._p("-" * 64)
+        for test in self._suite_status:
+            status = self._colored_status(test["test_status"])
+            self._p("\n%s - %s", test["test_name"], status)
+            for status, value in test["iterations"].iteritems():
+                self._p("%s%s: %i", self._indent, status, value)
+
     def _p(self, s, *args):
-        self._o.write(s % args + '\n')
+        self._o.write(s % args + "\n")
 
     def _colored_status(self, status):
         return self._in_color(status, status)
 
     def _in_color(self, color, s):
         if self._color:
-            return COLORS.get(color, '%s') % s
+            return COLORS.get(color, "%s") % s
         return s
