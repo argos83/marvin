54d44c226ec440f88be3bd6a2a76bbbdc839e55b
Seba <argos83@gmail.com>
2017-06-25 05:28:17 +1000

    feat: CLI option to print marvin version

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,8 @@
 import contextlib
 import os
+import sys
+
+from marvin.util import compat
 
 
 def resource(path):
@@ -19,3 +22,15 @@ def env_var(name, value):
             os.environ.pop(name)
         else:
             os.environ[name] = old_value
+
+
+class CaptureOutput(list):
+    def __enter__(self):
+        self._stdout = sys.stdout
+        sys.stdout = self._stringio = compat.string_io()
+        return self
+
+    def __exit__(self, *args):
+        self.extend(self._stringio.getvalue().splitlines())
+        del self._stringio
+        sys.stdout = self._stdout
