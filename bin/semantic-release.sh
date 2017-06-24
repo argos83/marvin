70cdfa78afe1083eb76e876afda30d33419b8634
argos83 <argos83@gmail.com>
2017-06-24 22:17:33 +1000

    chore: dont attempt release on branch builds (#19)

--- bin/semantic-release.sh
+++ bin/semantic-release.sh
@@ -1,5 +1,5 @@
 
-if [ "$TRAVIS_PULL_REQUEST" = false ] ; then
+if [ "$TRAVIS_PULL_REQUEST" = false ] && [ "$TRAVIS_BRANCH" = master ] ; then
     echo "Releasing new version of Marvin"
     git config --global user.name "semantic-release"
     git config --global user.email "semantic-release@travis"
