db159e4d07434d9054e6765fff353e161ffcf17b
Esteban Mendoza <estemendoza@gmail.com>
2017-11-26 13:17:28 +0000

    Including iteration information on the summary

--- marvin/report/observers/event_logger.py
+++ marvin/report/observers/event_logger.py
@@ -52,7 +52,8 @@ class EventLogger(object):
                             E.TEST_SETUP_ENDED,
                             E.TEST_ITERATION_ENDED,
                             E.TEST_TEARDOWN_ENDED)
-        publisher.subscribe(self.collect_iteration_status, E.TEST_ITERATION_ENDED)
+        publisher.subscribe(
+            self.collect_iteration_status, E.TEST_ITERATION_ENDED)
         publisher.subscribe(self.on_suite_ended, E.SUITE_ENDED)
 
         self._o = dest
@@ -61,7 +62,10 @@ class EventLogger(object):
         self._reset_test_summary()
 
     def collect_iteration_status(self, event):
-        self._current_test["iterations"][event.status] += 1
+        self._current_test["iterations_count"][event.status] += 1
+        self._current_test["iterations"].append(event)
+        if event.status != Status.PASS and event.status != Status.SKIP:
+            self._p("%s", self._format_exception(event.exception))
 
     def on_phase_started(self, event):
         title = PHASE_TITLES[event.event_type]
@@ -74,7 +78,9 @@ class EventLogger(object):
         phase_header = self._in_color('PHASE', title)
         phase_suffix = self._get_phase_suffix(event)
         status = self._colored_status(event.status)
-        self._p("%s[%s - %s]%s (%s)", INDENT, phase_header, status, phase_suffix, self._format_duration(event.duration))
+        self._p("%s[%s - %s]%s (%s)",
+                INDENT, phase_header, status, phase_suffix,
+                self._format_duration(event.duration))
 
     def on_step_started(self, event):
         step = event.step
@@ -85,7 +91,9 @@ class EventLogger(object):
         step = event.step
         status = self._colored_status(event.status)
         indent = INDENT * step.level
-        self._p("%s[%s] %s (%s)", indent, status, step.name, self._format_duration(event.duration))
+        self._p("%s[%s] %s (%s)",
+                indent, status, step.name,
+                self._format_duration(event.duration))
 
         if event.status == Status.SKIP and event.exception[0] == ContextSkippedException:
             self._p("%sSkip reason: %s", indent, event.exception[1].reason)
@@ -109,10 +117,9 @@ class EventLogger(object):
         self._current_test["status"] = event.status
 
         self._suite_status.append(self._current_test)
-        if event.status != Status.PASS:
-            for exc_info in event.exceptions:
-                self._current_test["exceptions"].append(self._format_exception(exc_info))
-        self._p("[%s - %s] %s (%s)", test_header, status, test_script.name, self._format_duration(event.duration))
+        self._p("[%s - %s] %s (%s)",
+                test_header, status, test_script.name,
+                self._format_duration(event.duration))
 
     def on_suite_ended(self, event):
         self._p("\n" + "-" * 64)
@@ -120,13 +127,16 @@ class EventLogger(object):
         self._p("-" * 64 + "\n")
         for test in self._suite_status:
             status = self._colored_status(test['status'])
-            ipass = test['iterations'][Status.PASS]
-            ifail = test['iterations'][Status.FAIL]
-            iskip = test['iterations'][Status.SKIP]
+            ipass = test['iterations_count'][Status.PASS]
+            ifail = test['iterations_count'][Status.FAIL]
+            iskip = test['iterations_count'][Status.SKIP]
             iall = ipass + ifail + iskip
             iteration_summary = "%d iteration(s) (%d pass - %d fail - %d skip)" % (iall, ipass, ifail, iskip)
             self._p("[%s] %s: %s", status, test['name'], iteration_summary)
-            [self._p("%s", exception) for exception in test['exceptions']]
+            for iteration in test["iterations"]:
+                iter_status = self._colored_status(iteration.status)
+                iter_name = iteration.iteration.name if iteration.iteration.name is not None else test['name']
+                self._p("    [%s] - %s (%s)", iter_status, iter_name, self._format_duration(iteration.duration))
 
     def _get_phase_suffix(self, event):
         suffix = ""
@@ -141,11 +151,12 @@ class EventLogger(object):
             "name": test_name,
             "status": None,
             "exceptions": [],
-            "iterations": {
+            "iterations_count": {
                 Status.PASS: 0,
                 Status.FAIL: 0,
                 Status.SKIP: 0
-            }
+            },
+            "iterations": []
         }
 
     def _p(self, s, *args):
